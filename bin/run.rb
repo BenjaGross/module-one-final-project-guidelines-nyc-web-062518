789948307795cccb3753a6428e42313b92091855
Tony Turetsky <TTuretsky@gmail.com>
2018-07-10 17:28:35 -0400

    typed some stuff

--- bin/run.rb
+++ bin/run.rb
@@ -1,11 +1,26 @@
 require_relative '../config/environment'
 
+game_on = true
+
 def run_game
   fake_region = Faker::ElderScrolls.region
-  puts "Welcome to #{fake_region}! Tell me, young adventurer, WHAT is your name!?"
+
+  puts "Old Wizard: Welcome to #{fake_region}! Tell me, Young-Adventurer, WHAT is your name!?"
   get_name = gets.chomp.to_s.capitalize
 
   story_hero = Player.new(:name => get_name , :race => "Wizard")
+
+  puts "Old Wizard: Ahhh, so YOU'RE #{story_hero.name}!"
+  puts "This land has been overrun by enemies of the Dark Lord. >:("
+
+  # game_loop
+end
+
+
+def game_loop
+  while game_on
+
+  end
 end
 
 run_game
